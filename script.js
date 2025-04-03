f6d506218e7a582652c2866da32b34292333e75b
Harald-Blokland <hablo81@gmail.com>
2025-04-03 12:49:03 +0200

    lagt til font og diverse andre endringer

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+const Soldiers = ["Spacemarine", "Terminator"]  
+console.log (Soldiers[0])
+let toString = Soldiers.toString()
+console.log(toString);
+let personOne = {
+    name: "Harald",
+    age: 43,
+    isLearning: true,
+}
+
+console.log(personOne.isTeaching);
